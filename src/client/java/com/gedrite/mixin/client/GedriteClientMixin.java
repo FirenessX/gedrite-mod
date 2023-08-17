c6b2164c46714248e1265335a0dde00c9ba2bfc8
FirenessX <unconfigured@null.spigotmc.org>
2023-08-18 00:40:11 +0400

    Gedrite Mod v1.0.0

--- src/client/java/com/gedrite/mixin/client/GedriteClientMixin.java
+++ src/client/java/com/gedrite/mixin/client/GedriteClientMixin.java
@@ -6,10 +6,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
 public class GedriteClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void run(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
-	}
+
 }
--- src/main/java/com/gedrite/mixin/GedriteMixin.java
+++ src/main/java/com/gedrite/mixin/GedriteMixin.java
@@ -6,10 +6,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
 public class GedriteMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+
 }
