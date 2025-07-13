d34e437e95c035949304eb88a8c4c8260463ce0d
unknown <gevorg.bazikyan.y@tumo.org>
2025-07-13 09:58:37 +0400

    Gedrite Mod v2.1.2 | 1.21.3

---
+++ Forge/1.21.3/src/main/java/com/gedrite/datagen/ModItemTagGenerator.java
@@ -0,0 +1,26 @@
+package com.gedrite.datagen;
+
+import com.gedrite.Gedrite;
+import com.gedrite.items.ModItems;
+import com.gedrite.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagGenerator extends ItemTagsProvider {
+
+    public ModItemTagGenerator(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pLookupProvider, CompletableFuture<TagLookup<Block>> pBlockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(pOutput, pLookupProvider, pBlockTags, Gedrite.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ModTags.Items.GEDRITE_ITEMS)
+                .add(ModItems.GEDRITE_INGOT.get(), ModItems.RAW_GEDRITE.get(), ModItems.GEDRITED_COAL.get(), ModItems.METAL_DETECTOR.get());
+    }
+}
---
+++ Forge/1.21.3/src/main/java/com/gedrite/mixins/FishingHookMixin.java
@@ -0,0 +1,36 @@
+package com.gedrite.mixins;
+
+import com.gedrite.fluids.ModFluidTypes;
+import com.gedrite.util.ModTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FishingHook;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
+
+@Mixin(FishingHook.class)
+public class FishingHookMixin {
+    @Inject(method = "shouldStopFishing", at = @At("HEAD"), cancellable = true)
+    private void gedrite$removeIfInvalid(Player player, CallbackInfoReturnable<Boolean> cir){
+        FishingHook hook = (FishingHook) (Object) this;
+        BlockPos blockpos = hook.blockPosition();
+        FluidState fluidstate = hook.level().getFluidState(blockpos);
+        if(hook.updateFluidHeightAndDoFluidPushing(ModTags.Fluids.GEDRITED_WATER, 0.014D)){
+            hook.discard();
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Unique
+    private static boolean updateMovementInFluid(Entity entity, Predicate<FluidState> shouldUpdate) {
+        entity.updateFluidHeightAndDoFluidPushing(shouldUpdate);
+        return entity.isInFluidType(ModFluidTypes.GEDRITED_WATER_FLUID_TYPE.get());
+    }
+}
